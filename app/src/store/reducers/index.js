11531840fd2913b429c80742c203296998fa02bb
victor dronov <victordronov1@gmail.com>
2020-09-09 16:18:51 -0400

    app setup?

---
+++ app/src/store/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  characters: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_CHARACTERS":
+      return {};
+  }
+};
