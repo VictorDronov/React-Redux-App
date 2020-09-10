7494db89eb3bddf1d8a00340644ae88071b63b40
victor dronov <victordronov1@gmail.com>
2020-09-10 15:12:53 -0400

    final edit famous last words

--- app/src/components/Character.js
+++ app/src/components/Character.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 const Character = (props) => {
+  const [show, setShow] = useState(false);
   const { details } = props;
-  console.log(details);
   return (
     <div className="characterCard">
-        <img alt={details.name} src={details.image}/>
-      <h3 className="name">{details.name}</h3>
-      <div>
-        <p>Species: {details.species}</p>&nbsp;
-        <p className={details.status}>Status: {details.status}</p>&nbsp;
-        <p>Gender: {details.gender}</p>&nbsp;
-        <p className="origin">Origin: {details.origin.name}</p>
+      <img alt={details.name} src={details.image} />
+      <h3 className="name">{details.name}</h3> &nbsp;
+      <div className="status">
+        <h4 className={details.status}>Status: {details.status}</h4>
+      </div>
+      <div className="showAndHide">
+        <div onClick={() => setShow(!show)}>
+          {!show ? (
+            <div className="showAndHide">Show Details</div>
+          ) : (
+            <div>
+              <div className="showAndHide"> Close Details</div>
+              <p>Species: {details.species}</p>
+              <p>Gender: {details.gender}</p>
+              <p className="origin">Origin: {details.origin.name}</p>
+              <p>Current Location: {details.location.name}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
